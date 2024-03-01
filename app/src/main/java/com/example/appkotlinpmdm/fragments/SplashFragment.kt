55a2e48ff4326687426f1e24f3303d652dd1d88e
Marcos García <markarules01@gmail.com>
2024-03-01 18:29:55 +0100

    Commit V22, modificado el SplashFragment para simular el tiempo de carga a 2 segundos.

--- app/src/main/java/com/example/appkotlinpmdm/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/appkotlinpmdm/fragments/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.example.appkotlinpmdm.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,11 +13,23 @@ import com.example.appkotlinpmdm.R
 class SplashFragment : Fragment() {
 
     private lateinit var navController: NavController
+    private lateinit var handler: Handler
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         navController = findNavController()
         return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        handler = Handler()
+
+        handler.postDelayed({
+            navController.navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 2000)
     }
 }
