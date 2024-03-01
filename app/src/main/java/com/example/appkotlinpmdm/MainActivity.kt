3bcb575c344c319673c6339d37b234d585e378ff
Marcos García <markarules01@gmail.com>
2024-03-01 12:40:28 +0100

    Commit V2, en relación a puntos 2 y 3 del proyecto, creación de funcion cambiaColor, que cambia el fondo del layoutMini a un color distinto cada vez mediante un random. Creacion del layoutMini para diferenciar del fondo principal, cambio de texto de holaMundo y adición del botón.

--- app/src/main/java/com/example/appkotlinpmdm/MainActivity.kt
+++ app/src/main/java/com/example/appkotlinpmdm/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.appkotlinpmdm
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var layoutMini: ConstraintLayout
+    lateinit var buttonCambiarColor: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutMini = findViewById(R.id.layoutMini)
+        buttonCambiarColor = findViewById(R.id.buttonCambiarColor)
+
+        buttonCambiarColor.setOnClickListener {
+            cambiaColor()
+        }
+    }
+
+    fun cambiaColor() {
+        val red = (0..255).random()
+        val green = (0..255).random()
+        val blue = (0..255).random()
+
+        val color = Color.rgb(red, green, blue)
+
+        layoutMini.setBackgroundColor(color)
     }
 }
