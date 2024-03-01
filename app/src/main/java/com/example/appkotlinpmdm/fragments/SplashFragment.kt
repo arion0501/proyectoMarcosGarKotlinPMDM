ff102f27a10bc6bfc80642a233092ab1a2322e72
Marcos García <markarules01@gmail.com>
2024-03-01 17:48:12 +0100

    Commit V18, creación de SplashFragment y correspondiente xml.

---
+++ app/src/main/java/com/example/appkotlinpmdm/fragments/SplashFragment.kt
@@ -0,0 +1,22 @@
+package com.example.appkotlinpmdm.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.appkotlinpmdm.R
+
+class SplashFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        navController = findNavController()
+        return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+}
