505182fda6c60e4e95ab5ca9f2737f520e25a010
Marcos García <markarules01@gmail.com>
2024-03-01 16:51:15 +0100

    Commit V14, modificados los fragments para que inicien los xml correctamente.

--- app/src/main/java/com/example/appkotlinpmdm/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/appkotlinpmdm/fragments/LoginFragment.kt
@@ -1,4 +1,22 @@
 package com.example.appkotlinpmdm.fragments
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.appkotlinpmdm.R
+
+class LoginFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        navController = findNavController()
+        return inflater.inflate(R.layout.login_fragment, container, false)
+    }
 }
--- app/src/main/java/com/example/appkotlinpmdm/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/appkotlinpmdm/fragments/RegisterFragment.kt
@@ -1,4 +1,22 @@
 package com.example.appkotlinpmdm.fragments
 
-class RegisterFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.appkotlinpmdm.R
+
+class RegisterFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        navController = findNavController()
+        return inflater.inflate(R.layout.register_fragment, container, false)
+    }
 }
